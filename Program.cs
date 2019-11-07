7e2c131ee6b80394ae62e035f525717ee654047e
Darel Johnson <dareljohnson@msn.com>
2019-11-07 01:37:56 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateItem().Wait();
+        }
+
+        private static async Task CreateItem()
+        {
+            var cosmosUrl =""; // add uri
+            var cosmoskey = ""; // secret key
+            var databaseName = "TestDB";
+            var containerName ="MyContainerName";
+            var partionKeyPath = "/partionKeyPath";
+            var RuPerSec = 400;
+
+            var client= new CosmosClient(cosmosUrl, cosmoskey);
+            Database database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            Container container = await database.CreateContainerIfNotExistsAsync(containerName,partionKeyPath,RuPerSec);
+
+            dynamic testItem = new {
+                id = Guid.NewGuid().ToString(),
+                partionKeyPath ="MyTestPkValue",
+                details ="it's working!"
+            };
+            var response = await container.CreateItemAsync(testItem);
+        }
+    }
+}
