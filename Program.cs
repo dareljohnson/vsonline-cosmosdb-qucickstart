1005da3d0053ced58fe9d693ada2cb8fd70aa25b
Darel Johnson <dareljohnson@msn.com>
2019-11-07 01:41:28 +0000

    First commit

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ namespace workspace
 
         private static async Task CreateItem()
         {
-            var cosmosUrl =""; // add uri
-            var cosmoskey = ""; // secret key
+            var cosmosUrl ="https://vso-cosmosdb.documents.azure.com:443/";
+            var cosmoskey = "7YmmJN39GvohOX1RkxNtX4LhbBNMTkWv3HhBkjEJmugBhCyKzhQhXUHpaXlD6M5knubuKzKFgqMdvdxA93lJLw==";
             var databaseName = "TestDB";
             var containerName ="MyContainerName";
             var partionKeyPath = "/partionKeyPath";
